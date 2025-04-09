edaf367b4e780b87a4f900277b2396ca152bf24d
Aakankshasingh29 <aakanksha29.singh@gmail.com>
2025-04-09 18:33:27 +0530

    button

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
 import Social from './components/Social';
+import Community from './components/community';
 
 function App() {
   return (
+    <div>
     <Social></Social>
- 
+    <Community></Community>
+    </div>
+   
   );
 }
 
---
+++ src/components/community.js
@@ -0,0 +1,30 @@
+import React from "react"
+import './community.css'
+
+const Community = () => {
+    return (
+      
+
+<div class="container mx-auto bg-gray-100 p-4 rounded-lg">
+  <h1 class="text-2xl font-bold mb-2">My Container</h1>
+  <p>This is some content inside the container.</p>
+  <div class="absolute top-0 right-0 size-5">
+    <div class="right-px">
+    <button className=" rounded-md border  border-slate-300 py-2 px-4 text-center text-sm transition-all shadow-sm hover:shadow-lg text-slate-600 hover:text-teal-400 hover:bg-black hover:border-slate-800 focus:text-black focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-teal-400 active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" >Default</button>
+    </div>
+  </div>
+  <div>
+  
+</div>
+</div>
+
+
+
+
+        
+
+ 
+    )
+}
+
+export default Community
