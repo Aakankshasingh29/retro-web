933fb79335e0e16b343855eaee4ba6921ae762a4
Aakankshasingh29 <aakanksha29.singh@gmail.com>
2025-04-15 18:36:46 +0530

    tue-15-april

--- src/components/community.js
+++ src/components/community.js
@@ -5,9 +5,9 @@ const Community = () => {
     return (
       
 
-<div class="container mx-auto bg-indigo-200 p-4 rounded-xl">
+<div class="container mx-auto bg-pink-100 p-4 rounded-xl ">
   <h1 class="text-2xl font-bold mb-2">Become a Creator</h1>
-  <p>At Retro Fusion,all our team members are known as Creators and we empower them to innovate, champion ground-breaking ideas, and bring courage – and fun – to work every day.</p>
+  <p className="font-medium  ">At Retro Fusion,all our team members are known as Creators and we empower them to innovate, champion ground-breaking ideas, and bring courage – and fun – to work every day.</p>
   <div class="flex-row-reverse">
     <div className=" px-2 py-2 m-4 text-right  top-0 right-0 space-x-4" >
     <button className=" rounded-full border border-black py-2 px-4 text-center text-sm transition-all shadow-sm hover:shadow-lg text-slate-600 hover:text-teal-400 hover:bg-black hover:border-slate-800 focus:text-black focus:bg-slate-800 focus:border-slate-800 active:border-slate-800 active:text-teal-400 active:bg-slate-800 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none" >Default</button>
@@ -16,23 +16,44 @@ const Community = () => {
     </div>
   </div>
 
-  <div className="container mx-md bg-purple-700 p-4 rounded-md">
-    <div className=""><h1>Grow from zeroto → oneto →one million.</h1></div>
-    <h2>Whether you’re just getting started on your creator journey or scaling your audience to new heights, Buffer will get your content in front of more people.</h2>
-    <div>
-    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-    <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
-    </svg>
-    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-    <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
-    </svg>
-    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512">
-    <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/>
-    </svg>
+<div className="grid grid-cols-2 gap-4 col-span-2  bg-purple-400 p-5 py-5 rounded-md">
+    <div className="text-xl">
+        <h1 class="text-2xl font-bold mb-2 font-serif">Grow from zeroto → oneto →one million.</h1>
+   
+    <h2 className="font-serif font-medium" >Whether you’re just getting started on your creator journey or scaling your audience to new heights, Buffer will get your content in front of more people.</h2>
     </div>
+    <br></br>
+    <div>
+    <ul>
+    <li class="inline-flex"> 
+    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"> 
+    <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/> 
+    </svg> 
+    <span className="font-serif font-medium"> Save all your ideas as inspiration strikes</span>
+    </li>
+    <br></br>
 
-    <div className="flex bg-purple-300 rounded-lg shadow dark:bg-gray-800 ">
-    <div class="container px-5 py-10 mx-auto">
+    <li class="inline-flex"> 
+    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"> 
+    <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/> 
+    </svg> 
+    <span className="font-serif font-medium"> Save all your ideas as inspiration strikes</span>
+    </li>
+    <br></br>
+
+    <li class="inline-flex"> 
+    <svg className="h-6 w-6" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"> 
+    <path d="M438.6 105.4c12.5 12.5 12.5 32.8 0 45.3l-256 256c-12.5 12.5-32.8 12.5-45.3 0l-128-128c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0L160 338.7 393.4 105.4c12.5-12.5 32.8-12.5 45.3 0z"/> 
+    </svg> 
+    <span className="font-serif font-medium"> Save all your ideas as inspiration strikes</span>
+    </li>
+    
+ 
+    </ul>
+</div>
+
+    <div className="  bg-purple-200 rounded-lg shadow dark:bg-gray-800 ">
+    <div class="container px-2 py-5 mx-sm">
         <h1 class="text-2xl font-semibold text-center text-gray-800 capitalize lg:text-3xl dark:text-white">Our Executive Team</h1>
 
         <p class="max-w-xl mx-auto my-6 text-center text-gray-800 dark:text-gray-300">
@@ -195,41 +216,9 @@ const Community = () => {
     </div>
 
     </div>
-
-
-
-
-
-
-
     
-
-
-    
-
- 
-
-
- 
-    
- 
-
-
-
-     
-      
-
-    
-   
-
-
-
-
-
-
-
-        
 </div>
+
  
     )
 }
